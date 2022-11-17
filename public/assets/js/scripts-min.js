4bcffab8ff087809bd3b3c5ffbb4fae9cf185424
kendollcastrom <kendoll@isppro.net>
2022-11-17 06:45:35 -0600

    update: logo anchor

--- public/assets/js/scripts-min.js
+++ public/assets/js/scripts-min.js
@@ -1,2 +1,63 @@
-!function(){return function e(t,n,r){function o(a,i){if(!n[a]){if(!t[a]){var u="function"==typeof require&&require;if(!i&&u)return u(a,!0);if(c)return c(a,!0);var s=new Error("Cannot find module '"+a+"'");throw s.code="MODULE_NOT_FOUND",s}var l=n[a]={exports:{}};t[a][0].call(l.exports,function(e){return o(t[a][1][e]||e)},l,l.exports,e,t,n,r)}return n[a].exports}for(var c="function"==typeof require&&require,a=0;a<r.length;a++)o(r[a]);return o}}()({1:[function(e,t,n){"use strict";function r(e,t){document.addEventListener("click",function(n){if(n.target.matches(e+" .a-btn"))if(n.target.parentElement.classList.contains("active"))n.target.parentElement.classList.remove("active");else{if(1==t){var r=document.querySelectorAll(e+" .a-container");Array.prototype.forEach.call(r,function(e){e.classList.remove("active")})}n.target.parentElement.classList.add("active")}})}Object.defineProperty(n,"__esModule",{value:!0}),r(".accordion.v1",!0),r(".accordion.v2",!1),n.default=r()},{}],2:[function(e,t,n){"use strict";Object.defineProperty(n,"__esModule",{value:!0});n.topNav=function(){document.querySelector(".hamburger").addEventListener("click",function(e){e.preventDefault(),[].map.call(document.querySelectorAll(".hamburger"),function(e){e.classList.toggle("is-active")}),[].map.call(document.querySelectorAll(".top-nav__menu"),function(e){e.classList.toggle("show-top-nav")})})}},{}],3:[function(e,t,n){"use strict";var r=e("./components/topNav"),o=e("./components/dropdown");(0,r.topNav)(),(0,o.initAcc)()},{"./components/dropdown":1,"./components/topNav":2}]},{},[3]);
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+function initAcc(elem, option) {
+    document.addEventListener('click', function (e) {
+        if (!e.target.matches(elem + ' .a-btn')) return;else {
+            if (!e.target.parentElement.classList.contains('active')) {
+                if (option == true) {
+                    var elementList = document.querySelectorAll(elem + ' .a-container');
+                    Array.prototype.forEach.call(elementList, function (e) {
+                        e.classList.remove('active');
+                    });
+                }
+                e.target.parentElement.classList.add('active');
+            } else {
+                e.target.parentElement.classList.remove('active');
+            }
+        }
+    });
+}
+initAcc('.accordion.v1', true);
+initAcc('.accordion.v2', false);
+exports.default = initAcc();
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+var topNav = exports.topNav = function topNav() {
+	var myFunction = function myFunction() {
+		document.querySelector('.hamburger').addEventListener('click', function (e) {
+			e.preventDefault();
+			[].map.call(document.querySelectorAll('.hamburger'), function (el) {
+				el.classList.toggle('is-active');
+			});
+			[].map.call(document.querySelectorAll('.top-nav__menu'), function (el) {
+				el.classList.toggle('show-top-nav');
+			});
+		});
+	};
+	myFunction();
+};
+
+},{}],3:[function(require,module,exports){
+'use strict';
+
+var _topNav = require('./components/topNav');
+
+var _dropdown = require('./components/dropdown');
+
+(function () {
+    (0, _topNav.topNav)();
+    (0, _dropdown.initAcc)();
+})();
+
+},{"./components/dropdown":1,"./components/topNav":2}]},{},[3]);
+
 //# sourceMappingURL=scripts-min.js.map
